2e76c7a963a5e84aa20df6200735284786c2448b
bbj1980 <bbj1980@gmail.com>
2022-09-18 11:00:38 +0530

    Update question.js

--- src/api/database/models/question/question.js
+++ src/api/database/models/question/question.js
@@ -10,11 +10,28 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: false,
             unique: true
         },
-        questioncategoryid: {
+        subjectid: {
             type: DataTypes.INTEGER,
             allowNull: false
         },
         questionTypeid: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        questionmarks: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        questiontime: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        isactive: {
             type: DataTypes.INTEGER,
             allowNull: false
         },
@@ -23,8 +40,6 @@ module.exports = (sequelize, DataTypes) => {
             defaultValue: true,
             allowNull: false
         }
-
-
     }, {
         freezeTableName: true
     });
